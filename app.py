ba0a4983b50d55e6eccfe9de119fb2da193e7bd6
Tom Harris <thomas.harris@smartbear.com>
2024-03-31 18:23:46 +0100

    Add /random route; move stl getters out to Icosahedron model

--- app.py
+++ app.py
@@ -2,15 +2,26 @@
 Simple Flask server to serve STL files for stellated icosahedra.
 """
 from flask import Flask, send_file
+from models import Icosahedron
 
 app = Flask(__name__)
 
 @app.route('/<icosahedron_id>')
-def serve_stl(icosahedron_id):
+def serve_icosahedron_stl(icosahedron_id):
     """
     Serve STL file for stellated icosahedron with given id.
     """
-    file_path = f"stl/icosahedra/{icosahedron_id}.stl"
+    file_path = Icosahedron.find(icosahedron_id)
+    if file_path is None:
+        return "Icosahedron not found", 404
+    return send_file(file_path, mimetype='model/stl')
+
+@app.route('/random')
+def serve_random_icosahedron_stl():
+    """
+    Serve STL file for a random stellated icosahedron.
+    """
+    file_path = Icosahedron.random()
     return send_file(file_path, mimetype='model/stl')
 
 if __name__ == '__main__':
